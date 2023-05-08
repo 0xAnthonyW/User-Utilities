321a53fb11a025f9cbe9531702576cf7a4795f4f
0xAnthonyW <123592619+0xAnthonyW@users.noreply.github.com>
2023-05-08 12:26:31 -0500

    Add project files.

---
+++ User-Utilities/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace User_Utilities
+{
+    partial class UserUtilities
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            output = new ListBox();
+            PWresetBtn = new Button();
+            PWexpireBtn = new Button();
+            RTclockBtn = new Button();
+            NetwrkBtn = new Button();
+            SuspendLayout();
+            // 
+            // output
+            // 
+            output.FormattingEnabled = true;
+            output.ItemHeight = 15;
+            output.Location = new Point(132, 5);
+            output.Name = "output";
+            output.Size = new Size(343, 259);
+            output.TabIndex = 0;
+            // 
+            // PWresetBtn
+            // 
+            PWresetBtn.Location = new Point(12, 5);
+            PWresetBtn.Name = "PWresetBtn";
+            PWresetBtn.Size = new Size(104, 25);
+            PWresetBtn.TabIndex = 1;
+            PWresetBtn.Text = "Password Reset";
+            PWresetBtn.UseVisualStyleBackColor = true;
+            PWresetBtn.Click += PWresetBtn_Click;
+            // 
+            // PWexpireBtn
+            // 
+            PWexpireBtn.Location = new Point(12, 36);
+            PWexpireBtn.Name = "PWexpireBtn";
+            PWexpireBtn.Size = new Size(104, 25);
+            PWexpireBtn.TabIndex = 2;
+            PWexpireBtn.Text = "Password Expire";
+            PWexpireBtn.UseVisualStyleBackColor = true;
+            PWexpireBtn.Click += PWexpireBtn_Click;
+            // 
+            // RTclockBtn
+            // 
+            RTclockBtn.Location = new Point(12, 67);
+            RTclockBtn.Name = "RTclockBtn";
+            RTclockBtn.Size = new Size(104, 25);
+            RTclockBtn.TabIndex = 3;
+            RTclockBtn.Text = "Real Time Clock";
+            RTclockBtn.UseVisualStyleBackColor = true;
+            RTclockBtn.Click += RTclockBtn_Click;
+            // 
+            // NetwrkBtn
+            // 
+            NetwrkBtn.Location = new Point(12, 98);
+            NetwrkBtn.Name = "NetwrkBtn";
+            NetwrkBtn.Size = new Size(104, 25);
+            NetwrkBtn.TabIndex = 4;
+            NetwrkBtn.Text = "Wi-Fi Reset";
+            NetwrkBtn.UseVisualStyleBackColor = true;
+            NetwrkBtn.Click += NetwrkBtn_Click;
+            // 
+            // UserUtilities
+            // 
+            AutoScaleMode = AutoScaleMode.Inherit;
+            ClientSize = new Size(487, 269);
+            Controls.Add(NetwrkBtn);
+            Controls.Add(RTclockBtn);
+            Controls.Add(PWexpireBtn);
+            Controls.Add(PWresetBtn);
+            Controls.Add(output);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "UserUtilities";
+            Text = "User Utilities";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListBox output;
+        private Button PWresetBtn;
+        private Button PWexpireBtn;
+        private Button RTclockBtn;
+        private Button NetwrkBtn;
+    }
+}
---
+++ User-Utilities/Form1.cs
@@ -0,0 +1,159 @@
+using System.Diagnostics;
+using System.DirectoryServices.AccountManagement;
+
+namespace User_Utilities
+{
+    public partial class UserUtilities : Form
+    {
+        public UserUtilities()
+        {
+            InitializeComponent();
+        }
+        private void PWresetBtn_Click(object sender, EventArgs e)
+        {
+            output.Items.Clear();
+
+            using (PrincipalContext context = new PrincipalContext(ContextType.Machine))
+            {
+                UserPrincipal userTemplate = new UserPrincipal(context)
+                {
+                    SamAccountName = "STU*"
+                };
+
+                using (PrincipalSearcher searcher = new PrincipalSearcher(userTemplate))
+                {
+                    foreach (UserPrincipal user in searcher.FindAll())
+                    {
+                        if (user.PasswordNeverExpires)
+                        {
+                            user.PasswordNeverExpires = false;
+                            user.Save();
+                        }
+                        user.ExpirePasswordNow();
+                        user.Save();
+
+                        output.Items.Add($"User '{user.SamAccountName}' password set to change on next login.");
+                    }
+                }
+            }
+        }
+
+        private void PWexpireBtn_Click(object sender, EventArgs e)
+        {
+            output.Items.Clear();
+
+            using (PrincipalContext context = new PrincipalContext(ContextType.Machine))
+            {
+                UserPrincipal userTemplate = new UserPrincipal(context)
+                {
+                    SamAccountName = "STU*"
+                };
+
+                using (PrincipalSearcher searcher = new PrincipalSearcher(userTemplate))
+                {
+                    foreach (UserPrincipal user in searcher.FindAll())
+                    {
+                        if (!user.PasswordNeverExpires)
+                        {
+                            user.PasswordNeverExpires = true;
+                            user.Save();
+                        }
+
+                        output.Items.Add($"User '{user.SamAccountName}' password set to never expire.");
+                    }
+                }
+            }
+        }
+
+        private void RTclockBtn_Click(object sender, EventArgs e)
+        {
+            string timezone = "Central Standard Time";
+            Process.Start("tzutil.exe", $"/s \"{timezone}\"").WaitForExit();
+            output.Items.Add($"Timezone set to {timezone}.");
+        }
+
+        private async void NetwrkBtn_Click(object sender, EventArgs e)
+        {
+            // Disable the button to prevent multiple clicks
+            NetwrkBtn.Enabled = false;
+
+            // Run the network reset on a separate Task to avoid freezing the UI
+            await Task.Run(() => ResetNetwork());
+
+            // Re-enable the button after the task is completed
+            NetwrkBtn.Enabled = true;
+        }
+
+        private void ResetNetwork()
+        {
+            // Restart the network adapter
+            ExecuteCommand("powershell", "Restart-NetAdapter -Name \"Wi*\"");
+            AppendOutput("Network adapter restarted");
+
+            // Clear DNS cache
+            ExecuteCommand("powershell", "Clear-DnsClientCache");
+            Thread.Sleep(5000);
+            ExecuteCommand("ipconfig", "/flushdns");
+            AppendOutput("Flushed DNS");
+
+            // Release and renew IP configuration
+            Thread.Sleep(5000);
+            ExecuteCommand("ipconfig", "/release");
+            Thread.Sleep(5000);
+            ExecuteCommand("ipconfig", "/renew");
+            AppendOutput("IP configuration renew & released!");
+
+            // Reset Winsock and network adapter
+            ExecuteCommand("netsh", "winsock reset");
+            Thread.Sleep(5000);
+            ExecuteCommand("netcfg", "-d");
+            AppendOutput("Network adapter has been reset");
+
+            // Prompt user to restart the computer
+            Thread.Sleep(4000);
+            DialogResult result = MessageBox.Show("Do you want to restart the computer now?", "Restart computer", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                ExecuteCommand("shutdown", "/r /f /t 0");
+            }
+        }
+
+        private void AppendOutput(string message)
+        {
+            // Invoke the UI thread to update the output ListBox
+            this.Invoke((Action)(() =>
+            {
+                output.Items.Add(message);
+
+                // If the message is "Network adapter has been reset", display the MessageBox
+                if (message == "Network adapter has been reset")
+                {
+                    DialogResult result = MessageBox.Show("Do you want to restart the computer now?", "Restart computer", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                    if (result == DialogResult.Yes)
+                    {
+                        ExecuteCommand("shutdown", "/r /f /t 0");
+                    }
+                }
+            }));
+        }
+
+        private void ExecuteCommand(string fileName, string arguments)
+        {
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = fileName,
+                    Arguments = arguments,
+                    RedirectStandardOutput = false,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+}
